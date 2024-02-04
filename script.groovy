7c1b060ea357eb64b7992d680cc4fe2e95c673b0
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-02-04 12:34:23 +0530

    Updated script.groovy

--- script.groovy
+++ script.groovy
@@ -10,7 +10,7 @@ def test_jar() {
 
 def build_image() {
     echo "build the docker image..."
-    sh "docker build -t ismailsdockers/java-maven-app:1.1.3 ."
+    sh "docker build -t ismailsdockers/java-maven-app:jma-2.0.1 ."
 }
 
 def push_image() {
@@ -18,7 +18,7 @@ def push_image() {
     
     withCredentials([usernamePassword(credentialsId: 'docker-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push ismailsdockers/java-maven-app:1.1.3"
+        sh "docker push ismailsdockers/java-maven-app:jma-2.0.1"
     }
 }
 
