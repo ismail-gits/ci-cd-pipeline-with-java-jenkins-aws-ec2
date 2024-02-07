2fdaa514306e03f028ad9f16a96e3e0034b8fbf5
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-02-07 19:00:19 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -39,19 +39,20 @@ def deployImage() {
 def commitVerisonUpdate() {
     echo "committing version update to git repo..."
 
+    sh 'git config --global user.email "jenkins@example.com"'
+    sh 'git config --global user.name "jenkins"'
+
+    sh "git status"
+    sh "git branch"
+    sh "git config --list"
+
     withCredentials([usernamePassword(credentialsId: 'gitlab-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'git config --global user.email "jenkins@example.com"'
-        sh 'git config --global user.name "jenkins"'
-
-        sh "git status"
-        sh "git branch"
-        sh "git config --list"
-
         sh "git remote set-url origin https://$USER:$PASS@gitlab.com/ismailGitlab/jenkins-pipeline-java-maven.git"
-        sh "git add ."
-        sh "git commit -m 'jenkins-ci: version bump'"
-        sh "git push origin HEAD:main"
     }
+
+    sh "git add ."
+    sh "git commit -m 'jenkins-ci: version bump'"
+    sh "git push origin HEAD:main"
 }
 
 return this
