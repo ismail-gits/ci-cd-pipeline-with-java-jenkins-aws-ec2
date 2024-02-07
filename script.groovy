1704a7d308c0b1addc2645060394c97c248a9a70
Mohammed Ismail <mohammedismail72425@gmail.com>
2024-02-07 22:36:07 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -47,10 +47,11 @@ def deployImage() {
 
     // Deploying and running image with postgres docker image using docker-compose (multiple containers)
     def shellCmd = "bash ./ec2-commands.sh $IMAGE_NAME"
+    def ec2Instance = "c2-user@65.2.70.3"
     sshagent(['EC2-server-key']) {
-        sh "scp docker-compose.yaml ec2-user@65.2.70.3:/home/ec2-user"
-        sh "scp ec2-commands.sh ec2-user@65.2.70.3:/home/ec2-user"
-        sh "ssh -o StrictHostKeyChecking=no ec2-user@65.2.70.3 $shellCmd"
+        sh "scp docker-compose.yaml $ec2Instance:/home/ec2-user"
+        sh "scp ec2-commands.sh $ec2Instance:/home/ec2-user"
+        sh "ssh -o StrictHostKeyChecking=no $ec2Instance $shellCmd"
     }
 }
 
